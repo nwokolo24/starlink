8522c0ed8a0e152e647f8023e654ea1e1c69fdf8
dmalasky <dmalasky@byui.edu>
2022-01-29 15:02:51 -0700

    added the card class

---
+++ Hilo/Card.cs
@@ -0,0 +1,15 @@
+//Handles drawing cards.
+class Card
+{
+    public int draw1;
+    public int draw2;
+
+    //Method to generate a random card.
+    public int randomCard()
+    {
+        Random numberGen = new Random();
+        int cardNum = numberGen.Next(1, 13);
+        return cardNum;
+    }
+
+}
